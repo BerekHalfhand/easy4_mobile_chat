881b8f7c0e6465e02a47963b2ac74c085704cb95
Ilnur <ilnuribat@gmail.com>
2019-06-10 11:51:01 +0300

    test doesnt work, but compiled

--- src/response.js
+++ src/response.js
@@ -1,11 +1,14 @@
-export const success = (payload) => ({
+const success = (payload) => ({
   statusCode: 200,
   result: payload
-})
+});
 
-export const failure = (errors) => ({
+const failure = (errors) => ({
   statusCode: 500,
   ...errors
-})
+});
 
-export default {success, failure};
+module.exports = {
+  success,
+  failure,
+};
--- tests/setup.js
+++ tests/setup.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 const mongooseController = require('../src/controllers/mongoose');
-// const rabbitController = require('./../src/controllers/rabbit');
-// const { rpcHandler } = require('./../src/handlers/rpc');
 
 beforeAll(async () => {
   await mongooseController.init(() => {});
-  // await rabbitController.init(rpcHandler);
-  // await Promise.delay(3000);
 });
 
 
